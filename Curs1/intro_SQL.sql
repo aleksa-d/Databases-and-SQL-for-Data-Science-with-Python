e8f89dc4f3cd59c909ccb42d6bb65d2db7b63c13
Alexandra Voicu <aleksandra.voicu@gmail.com>
2025-03-19 17:02:38 +0200

    Instructiuni introductive SQL

---
+++ Curs1/intro_SQL.sql
@@ -0,0 +1,26 @@
+SHOW DATABASES;
+
+-- CREATE DATABASE prima_baza_de_date;
+
+CREATE DATABASE a_doua_baza_de_date;
+
+DROP DATABASE	a_doua_baza_de_date;
+
+CREATE DATABASE IF NOT EXISTS a_treia_baza_de_date;
+
+DROP DATABASE IF EXISTS a_treia_baza_de_date;
+
+show databases;
+
+USE 	prima_baza_de_date;
+CREATE TABLE calorii (nr_crt INT, calorii INT);
+
+INSERT INTO calorii VALUES (1, 2500);
+
+SELECT * FROM 	calorii;
+INSERT INTO calorii SET nr_crt=4, calorii=1900;
+SELECT * FROM 	calorii;
+
+INSERT INTO calorii SET calorii=900;
+SELECT * FROM 	calorii;
+
